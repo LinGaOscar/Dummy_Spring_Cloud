8f1ebb5f754912da3fc9a7e643012589a36a865f
Oscar <oscar987123@yahoo.com.tw>
2024-03-15 11:13:45 +0800

    reset pom

--- DummySpringCloud/DummyDiscovery/src/main/java/com/oscar/dummyserver/DummyServerApplication.java
+++ DummySpringCloud/DummyDiscovery/src/main/java/com/oscar/dummyserver/DummyServerApplication.java
@@ -2,7 +2,9 @@ package com.oscar.dummyserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DummyServerApplication {
 
